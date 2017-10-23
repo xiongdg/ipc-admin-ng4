c52537c0c497f6146e4c31ee5955352dc458d3e7
zhiq <865077695@qq.com>
2017-10-23 10:51:49 +0800

    nothing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { AccesstokenService } from './service/accesstoken.service';
 import { HttpService } from './service/http.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StoreService } from './service/store.service';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +13,29 @@ export class AppComponent {
   title = 'app';
   constructor(
     private httpService: HttpService,
-    private accesstokenService: AccesstokenService
+    private accesstokenService: AccesstokenService,
+    private router: Router,
+    private storeService: StoreService,
   ) {
 
   }
 
   ngOnInit() {
+    // 自动登录
+    // this.httpService.getData('admin/login.do', {userName: 'admin', password: 'admin'})
+    // .subscribe(data => {
+    //   console.log(1);
+    //   if (data.code === 200) {
+    //     // 将登录标识设置为true、路由跳转至内容页
+    //     this.storeService.setItem('accessToken', 1);
+    //     this.router.navigate(['user-list']);
+    //   }
+    // });
     // console.clear();
     // 第一时间判断登录状态、设置isloggedin值
     this.httpService.getData('admin/checkLogin.do', '')
       .subscribe(res => {
+        console.log(2);
         if (res.data.isLogin === 1) {
           this.accesstokenService.accessToken = true;
         } else if (res.data.isLogin === 0) {
--- src/app/gard/auth-gard.guard.ts
+++ src/app/gard/auth-gard.guard.ts
@@ -16,7 +16,7 @@ export class AuthGardGuard implements CanActivate, CanActivateChild {
   ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    // 再此处判断登录状态。如果未登录将路由导至登录路由。已登录则判断是否在调往登录，如果是则导至首页
+    // 再此处判断登录状态。如果未登录将路由导至登录路由。已登录则判断是否在跳往登录，如果是则导至首页
     const url: string = state.url;
     return this.checkLogin(url);
   }
@@ -30,6 +30,7 @@ export class AuthGardGuard implements CanActivate, CanActivateChild {
       this.router.navigate(['user-list']);    // 重定向至首页
       return false;
     }
+    // 在路灯系统中，关闭未登录检测。避免显示登录页。做自动登录
     if (url !== '/user/login' && this.storeService.getItem('accessToken') === '0') {   // 对于非登录页，如果未登录则阻止
       this.router.navigate(['user/login']);    // 重定向至登录页
       return false;
